330bb6374776804cdb96905bf1fd39426d506213
HiImZsofi <zsofia.gyori00@gmail.com>
2022-11-08 10:17:33 +0100

    masodik feladat

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 let userlista = []
+let unames = []
+let emails = []
+let telefon = []
 
 document.addEventListener('DOMContentLoaded', () => {
 
     document.getElementById('names').addEventListener('click', async () => {
         usernames()
     })
-
+    document.getElementById('contact').addEventListener('click', async () => {
+        table()
+    })
 })
 
 
@@ -22,7 +27,34 @@ async function usernames(){
         let li = document.createElement('li')
         li.textContent = p.lastName.toUpperCase() + ", " + p.firstName
         document.getElementById("htmllista").appendChild(li);
+        unames.push(p.maidenName)
+        emails.push(p.email)
+        telefon.push(p.phone)
     }
 
+}
+
+async function table(){
+    let response = await fetch('users.json')
+    let result = await response.json()
+
+  result.users.sort((a, b) => a.username.localeCompare(b.username))
+
+
+    for (let u of result.users){
+        
+        let tr = document.createElement('tr');
+        let td1 = document.createElement('td');
+        let td2 = document.createElement('td');
+        let td3 = document.createElement('td');
+        td1.textContent = u.username;
+        td2.textContent = u.email;
+        td3.textContent = u.phone;
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tabla.appendChild(tr);
+    }
+    
 }
 
